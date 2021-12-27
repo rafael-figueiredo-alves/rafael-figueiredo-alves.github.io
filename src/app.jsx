a80b1b2f849977ba183b3b70d7708fcfd5c059c4
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-12-27 16:16:02 -0300

    Update 05 - 27/12/2021

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './header';
 
 function App(){
     return <div>
-             <h1>Portfólio do Rafael Alves</h1>
+             <Header></Header>
+             <h1>Portfólio do Rafael de Figueiredo Alves</h1>
            </div>
 }
 
