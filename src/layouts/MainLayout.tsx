af82ade67522d50a191fe1575af2811a04f1eef8
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-01-21 23:03:31 -0300

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,114 +9,43 @@ function MainLayout() {
 
   return (
     <>
-{/*       <div>
-        <a href="https://github.com/rafael-figueiredo-alves" target="_blank">
-          <img src={Logo} classNameName="logo" alt="LogoRafael" />
-        </a>
-      </div>
-      <h1>Portfólio "Rafael de Figueiredo Alves"</h1>
-      <div classNameName="card">
-        <CountButton Inicio={10} />
-        <p>
-          Edite <code>src/App.tsx</code> e salve para testar HMR
-        </p>
-      </div>
-      <p>
-        Página em construção...
-        <Link to="/contacts">Contatos</Link>
-        <Outlet />
-      </p> */}
-
-<div className="container-fluid overflow-hidden">
-    <div className="row vh-100 overflow-auto">
-        {/* Este é o componente que abriga tanto a barra lateral quanto a NavBar */}
-        <div className="col-12 col-md-5 col-lg-4 col-xl-3 px-sm-2 px-0 d-flex sticky-top row-height" style={{backgroundColor: '#336699'}}>
+      <div className="container-fluid overflow-hidden">
+        <div className="row vh-100 overflow-auto">
+          {/* Este é o componente que abriga tanto a barra lateral quanto a NavBar */}
+          <div className="d-none d-md-table-cell col-md-5 col-lg-4 col-xl-3 px-sm-2 px-0 d-flex sticky-top row-height" style={{ backgroundColor: '#336699' }}>
             <div className="d-flex flex-sm-column flex-row flex-grow-1 align-items-center align-items-sm-start px-3 pt-2 text-white row-height">
               <div className='d-none d-md-inline'>
                 Mostrando sidebar em telas médias a grandes
               </div>
-              <div className='d-inline d-md-none'>
-                <h4 className='text-center'>Barra de navegação</h4>
-              </div>
-{/*                 <a href="/" className="d-flex align-items-center pb-sm-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span className="fs-5">B<span className="d-none d-sm-inline">rand</span></span>
-                </a>
-                <ul className="nav nav-pills flex-sm-column flex-row flex-nowrap flex-shrink-1 flex-sm-grow-0 flex-grow-1 mb-sm-auto mb-0 justify-content-center align-items-center align-items-sm-start" id="menu">
-                    <li className="nav-item">
-                        <a href="#" className="nav-link px-sm-0 px-2">
-                            <i className="fs-5 bi-house"></i><span className="ms-1 d-none d-sm-inline">Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-sm-0 px-2">
-                            <i className="fs-5 bi-speedometer2"></i><span className="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-                    </li>
-                    <li>
-                        <a href="#" className="nav-link px-sm-0 px-2">
-                            <i className="fs-5 bi-table"></i><span className="ms-1 d-none d-sm-inline">Orders</span></a>
-                    </li>
-                    <li className="dropdown">
-                        <a href="#" className="nav-link dropdown-toggle px-sm-0 px-1" id="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i className="fs-5 bi-bootstrap"></i><span className="ms-1 d-none d-sm-inline">Bootstrap</span>
-                        </a>
-                        <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdown">
-                            <li><a className="dropdown-item" href="#">New project...</a></li>
-                            <li><a className="dropdown-item" href="#">Settings</a></li>
-                            <li><a className="dropdown-item" href="#">Profile</a></li>
-                            <li>
-                                <hr className="dropdown-divider" />
-                            </li>
-                            <li><a className="dropdown-item" href="#">Sign out</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" className="nav-link px-sm-0 px-2">
-                            <i className="fs-5 bi-grid"></i><span className="ms-1 d-none d-sm-inline">Products</span></a>
-                    </li>
-                    <li>
-                        <a href="#" className="nav-link px-sm-0 px-2">
-                            <i className="fs-5 bi-people"></i><span className="ms-1 d-none d-sm-inline">Customers</span> </a>
-                    </li>
-                </ul>
-                <div className="dropdown py-sm-4 mt-sm-auto ms-auto ms-sm-0 flex-shrink-1">
-                    <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="hugenerd" width="28" height="28" className="rounded-circle" />
-                        <span className="d-none d-sm-inline mx-1">Joe</span>
-                    </a>
-                    <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                        <li><a className="dropdown-item" href="#">New project...</a></li>
-                        <li><a className="dropdown-item" href="#">Settings</a></li>
-                        <li><a className="dropdown-item" href="#">Profile</a></li>
-                        <li>
-                            <hr className="dropdown-divider" />
-                        </li>
-                        <li><a className="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
-                </div> */}
             </div>
-        </div>
-        {/* Conteúdo principal da página */}
-        <div className="col d-flex flex-column h-sm-100">
+          </div>
+          {/* Conteúdo principal da página */}
+          <div className="col d-flex flex-column h-sm-100">
             <main className="row overflow-auto">
-                <div className='row d-none d-md-inline'>
-                  Teste
+              <nav className="navbar d-inline d-md-none py-3 fixed-top" style={{ backgroundColor: '#336699', color: 'white', position: 'absolute', backgroundBlendMode: 'normal', boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2),0px 1px 10px rgba(0,0,0,0.12),0px 4px 5px rgba(0,0,0,0.14)'  }}>
+                <div className="container-fluid">
+                  <span className="navbar-brand mb-0 h1">Navbar</span>
                 </div>
-                <Outlet />
-                <div className="col pt-4">
-                    <h3>Vertical Sidebar that switches to Horizontal Navbar</h3>
-                    <p className="lead">An example multi-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single menu is be open at a time.</p>
-                    <hr />
-                    <h3>More content...</h3>
-                    <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
-                    <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
-                </div>
+              </nav>
+              <div className='row d-none d-md-inline'>
+                Teste
+              </div>
+              <Outlet />
+              <div className="col pt-4">
+                <h3>Vertical Sidebar that switches to Horizontal Navbar</h3>
+                <p className="lead">An example multi-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single menu is be open at a time.</p>
+                <hr />
+                <h3>More content...</h3>
+                <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+                <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
+              </div>
             </main>
             <footer className="row bg-light py-4 mt-auto">
-                <div className="col"> Footer content here... </div>
+              <div className="col"> Footer content here... </div>
             </footer>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
     </>
   )
 }
