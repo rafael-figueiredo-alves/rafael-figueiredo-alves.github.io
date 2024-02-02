031a3a7d4ee8e435fb21a685bbe3396264360a5e
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-02-01 23:03:53 -0300

    Update February 1, 2024
    => Working with data

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -8,17 +8,36 @@ import { iProjects } from '../models/ProjectsModel'
 const PageAbout = () => {
     const { CurrentLanguage, Translate } = useContext(LanguageContext)
     const [projects, setProjects] = useState<iProjects[]>([])
+    const [indice, setIndice] = useState<number>(0);
 
     useEffect(() => { GetDataFromJSONService<iProjects>('projects', CurrentLanguage).then(data => setProjects(() => data)) }, [CurrentLanguage])
+
+    const Anterior = () => {
+        if(indice > 0) {
+            setIndice(indice - 1);
+        }
+    }
+
+    const Proximo = () => {
+        if(indice < projects.length-1){
+            setIndice(indice + 1);
+        }
+    }
 
     return (
         <>
             <PageTitle Image={PageAboutImg}>{Translate("About.Titulo", false)}</PageTitle>
-            <ul>
+{/*             <ul>
                 {projects.map((project, index) => (
                     <ProjectForm data={project} num={index} />
                 ))}
-            </ul>
+            </ul> */}
+            <button  className='btn btn-primary' onClick={Anterior} disabled={indice == 0}>Anterior</button>
+            <button  className='btn btn-primary' onClick={Proximo} disabled={indice == projects.length-1}>Próximo</button>
+            {projects.length > 0 && (
+                <ProjectForm2 data={projects} num={indice} />
+            )}
+
         </>
     )
 }
@@ -30,6 +49,16 @@ const ProjectForm = ({ data, num }: { data: iProjects, num: number }) => {
             <p>- {data.Version}</p>
             <p>- {data.Description}</p>
         </li>
+    )
+}
+
+const ProjectForm2 = ({ data, num }: { data: iProjects[], num: number }) => {
+    return (
+        <>
+            <p>- {data[num].Title}</p>
+            <p>- {data[num].Version}</p>
+            <p>- {data[num].Description}</p>
+            </>
     )
 }
 
