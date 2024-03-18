83b6f253fcedc3966de7858744faf82e89fdb129
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2024-03-17 22:55:38 -0300

    Update March 17, 2024
    => Finished About's page

--- src/contexts/Translations.tsx
+++ src/contexts/Translations.tsx
@@ -1,4 +1,4 @@
-export const Translations = {
+export const Translations = {  
     "pt-BR" : {
         "Menu" :{
             "Inicio"      : "Inicio",
@@ -17,7 +17,13 @@ export const Translations = {
             "BoasVindas" : "Olá! Seja bem vindo ao meu portfólio. Sou desenvolvedor fullstack e gostaria, com esse site, de compartilhar meus projetos, minhas ideias, ferramentas que uso, linguagens e frameworks que uso e estudo e minha experiência e habilidades."
         },
         "About":{
-            "Titulo" : "Sobre mim"
+            "Titulo" : "Sobre mim",
+            "ParagrafoInicial" : 
+                "Olá! Meu nome é <strong>Rafael de Figueiredo Alves</strong>. Sou desenvolvedor de sistemas e trabalho na RZ Sistemas. Tenho dois filhos e uma esposa que são minha inspiração. Sou de São Bernardo do Campo e amo programação desde meu 12 anos, quando tive contato com a área por conta do meu irmão Miguel que trouxe um CD de Delphi que me fascinou por sua simplicidade. Além disso, meu irmão também mexia em Clipper, o que eu achava fascinante pelo fato de poder criar coisas usando um computador. Sempre gostei de criar, muito por conta de brincar com lego, assim como também gostava de games. Fui da época de Master System, Mega drive e Super Nintendo, mas meu console favorito hoje é o Playstation. No meu tempo livre, além de estudar, gosto de jogar com minha esposa e filhos.",
+            "ParagrafoLinguagens" : 
+                "Iniciei minha trilha no mundo do desenvolvimento com a linguagem Clipper, mas infelizmente não cheguei a concluir nenhum projeto na época. Depois conheci a linguagem que mais construí softwares: o Delphi. A linguagem pascal me dominou com sua simplicidade e a IDE tornou possível diversos projetos de forma muito simples e prática. Aprendi muito assistindo grandes mestres como Adriano Santos e Thulio Bittencourt. Hoje, vivo uma nova onda de aprendizado, tentando dominar Rust, Python e Dart. Trabalhando bastante com C Sharp e Blazor, posso dizer que meus conhecimentos foram ampliados na área de programação com mestres como Balta e Macoratti. Não posso deixar de mencionar o Rodrigo Zimmermann, CEO da empresa que trabalho e que me proporciona diversas chances de aprender e desenvolver meus conhecimentos ainda mais.",
+            "ParagrafoTrabalhos" : 
+                "Falando de trabalho, me formei em Letras - Português/Inglês e trabalhei cerca de 15 anos como professor de língua inglesa, sendo 13 deles na escola onde aprendi o idioma: o CCAA. A oportunidade de trabalhar com meus ex-professores no Centro Paulista de Cultura Anglo Saxônica, Razão social da empresa que me contratou e que era na minha opinião a maior e melhor franqueada do CCAA, me proprocionou não só conhecimento na área de ensino, como também abriu portas para minhas ideias de Software que auxiliaram nossos trabalhos nessa caminhada. Infelizmente, em 2020 encerraram as atividades devido ao falecimento da dona do grupo. No ano seguinte trabalhei na unidade de Jabaquara e tomei coragem de seguir meu verdadeiro sonho: ser desenvolvedor de software. Foi quando, no início de 2022, num Valentine's Day, comecei a trabalhar remotamente para a RZ Sistemas. Dando suporte ao ERP da empresa, trabalhei em integrações com e-commerces como Shopify, Wake, NuvemShop e Loja Integrada. Também tive oportunidade de trabalhar com a MDFe. Hoje, nós desenvolvemos o futuro da empresa com a linguagem C Sharp e o fantástico framework web chamado Blazor." 
         },
         "Contacts":{
             "Titulo" : "Entre em contato",
@@ -102,7 +108,13 @@ export const Translations = {
             "BoasVindas" : "Hi! Welcome to my portfolio. I'm a fullstack developer and I'd like, with this site, to share my projects,ideas, tools I use, languages and frameworks I use at work and study about and, also, my experience and skills."
         },
         "About":{
-            "Titulo" : "About me"          
+            "Titulo" : "About me",
+            "ParagrafoInicial" : 
+                "Hi! My name's <strong>Rafael de Figueiredo Alves</strong>. I'm a software developer and I work at RZ Sistemas. I have 2 kids and a wife who are my inspiration. I'm from São Bernardo do Campo and I've fallen in love with programming and development world since I was 12, when I got in touch with the area becuase of my brother Miguel that brought a CD of Delphi home and it amused me by its simplicity. Besides, my brother used to work with Clipper, what fascinated me because of the fact you can build amazing things using a computer. I've always enjoyed building things, much becuase I liked to playt with Lego, and I also enjoyed playning games. I was from Master System's, Mega drive's and Super Nintendo's era, but my favorite console today is Playstation. In my free time, besides studying, I like to play games with my wife and kids.",
+            "ParagrafoLinguagens" : 
+                "I started my journey in programming world with Clipper, however, unfortunatelly I didn't finish any project at the time. Then I was introduced to the language that I've built many softwares: Delphi.The Pascal Language made me fall in love with it because of its simplicity and its IDE made manby of my projects possible in a really simple and sensible way. I learned a lot buy watching Delphi's masters like Adriano Santos and Thulio Bittencourt. Today, I am on a new vibe learning new techs like Rust, Python and Dart. Working a lot with C Sharp and Blazor, I can say my knowledge has been largely amplified with masters like Balta and Macoratti. I also have to mention Rodrigo Zimmermann, CEO at my workplace. He provides me many opportunities to learnand improve my skills even more.",
+            "ParagrafoTrabalhos" : 
+                "Talking about work, I have a major in Languages and I worked for about 15 years like an English teacher, 13 of then at the school where I learned the English language: CCAA. The opportunity to work with my ex-teachers at Centro Paulista de Cultura Anglo Saxônica, the Company name of the workplace that hired me and that, in my opinion, the biggest and best CCAA branch, letting me learn not only about teaching, but also gave me support with some ideas of software that helped our work. Unfortunatelly, in 2020 they ended their activity due to the owner's death. The next year I worked in a branch in Jabaquara and I got the courage to follow my dream: be a software developer. That was ehn, in the eraly 2022, in a  Valentine's Day, I started to work remotelly at RZ Sistemas. Giving maintance to company's ERP, I've already woked with integrations with e-commerces like Shopify, Wake, NuvemShop and Loja Integrada. I've also got the chance to wirk with Brazilian's MDFe. Today, we develop the company's future with C Sharp and the amazing web framework called Blazor."      
         },
         "Contacts":{
             "Titulo" : "Keep in touch",
--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -3,6 +3,9 @@ import PageAboutImg from '/src/assets/PageTitleImg/PageAboutImg.png'
 import { LanguageContext } from '../contexts/LanguageContext'
 import { PageTitle } from '../components/Shared/PageTitle'
 import { PageTitleContext, Pages } from '../contexts/PageTitleContext'
+import MyPhoto from '/src/assets/MyPhoto.png'
+import Languages from '/src/assets/Languages.png'
+import Jobs from '/src/assets/Jobs.png'
 
 const PageAbout = () => {
     const { Translate } = useContext(LanguageContext);
@@ -14,6 +17,36 @@ const PageAbout = () => {
         <>
             <div className='d-md-none'>
                 <PageTitle Image={PageAboutImg}>{Translate("About.Titulo", false)}</PageTitle>
+            </div>
+            
+            <div className='container-fluid row'>
+                <div className='col-12 col-md-4 text-center'>
+                    <img src={MyPhoto} height={200} width={200} alt='Developer' />
+                    <figcaption><strong>Rafael de Figueiredo Alves</strong></figcaption>
+                </div>
+                <div className='col-12 col-md-8'>
+                    {Translate("About.ParagrafoInicial", true)}
+                </div>
+            </div>
+            
+            <div className='container-fluid row mt-4'>
+            <div className='col-12 col-md-8'>
+                    {Translate("About.ParagrafoLinguagens", true)}
+                </div>
+                <div className='col-12 col-md-4 text-center'>
+                    <img src={Languages} height={200} width={200} alt='Languages' />
+                    <figcaption><strong>Delphi, C Sharp, Blazor, Lazarus</strong></figcaption>
+                </div>
+            </div>
+
+            <div className='container-fluid row mt-4 mb-5'>
+                <div className='col-12 col-md-4 text-center'>
+                    <img src={Jobs} height={200} width={200} alt='Jobs' />
+                    <figcaption><strong>CCAA, RZ Sistemas</strong></figcaption>
+                </div>
+                <div className='col-12 col-md-8'>
+                    {Translate("About.ParagrafoTrabalhos", true)}
+                </div>
             </div>
         </>
     )
